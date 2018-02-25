9c9cccde383597dbc8e867422f5abf121d1a5ae9
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-02-25 06:52:26 +0100

    add a few more aliases

--- src/aliases.cmd
+++ src/aliases.cmd
@@ -1,5 +1,25 @@
 @echo off
 
+doskey g=git $*
+
+doskey ga=git add .
+
+doskey gb=git branch
+
 doskey gc=git checkout $*
 
 doskey gcd=git checkout develop
+
+doskey gd=git diff
+
+doskey gi=git init
+
+doskey gm=git merge $*
+
+doskey glog=git log --oneline --decorate --color --graph
+
+doskey ggpull=git pull
+
+doskey ggpush=git push
+
+doskey gs=git status
